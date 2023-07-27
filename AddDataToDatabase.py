41a1cad807b4462323bf91576270ecc5c38cecb1
Nayjel9 <christianervintan9@gmail.com>
2023-07-27 11:02:02 +0800

    new

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -4,17 +4,18 @@ from firebase_admin import db
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL':"https://realtimecriminaldetection-default-rtdb.firebaseio.com/"
+    'databaseURL': "https://realtimecriminaldetection-default-rtdb.firebaseio.com/"
 })
 
 ref = db.reference('Criminals')
 
 data = {
-    "18002215500":
+    "312654":
         {
             "name": "Christian Ervin T. Tan",
-            "Criminal No.": "18002215500"
+            "Criminal No.": "312654"
         }
+
 }
 
 for key, value in data.items():
